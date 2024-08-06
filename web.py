af8549851defcdd998fe84e40cd1879bd98984f7
Roy Moore <roy@moore.co.il>
2024-08-06 16:01:23 +0300

    Refactor web flow

--- web.py
+++ web.py
@@ -3,23 +3,24 @@ from js import console
 
 from app import word_count, create_table, table_to_html
 
+async def process_data(data: str):
+    console.log(f"Processing file content...")
+    results = await word_count(data)
+    table = await create_table(results, count=10)
+    html_table = await table_to_html(table)
+    # update the document with the table.
+    document.getElementById("output").innerHTML = html_table
+
 async def on_change(event):
     # For each file the user has selected to upload...
     for file in input.files:
-        console.log(f"Processing {file.name}...")
-        console.log(f"File size: {file.size} bytes")
-        console.log(f"File type: {file.type}")
+        # console.log(f"Processing {file.name}...")
+        # console.log(f"File size: {file.size} bytes")
+        # console.log(f"File type: {file.type}")
         console.log(f"Reading file content...")
         # Read the file content as a string.
         content = await file.text()
-        # Count the words in the file content.
-        results = word_count(content)
-        # Create a table with the top 10 words.
-        table = create_table(results, count=10)
-        # Convert the table to an HTML table.
-        html_table = table_to_html(table)
-        # update the document with the table.
-        document.getElementById("output").innerHTML = html_table
+        await process_data(content)
 
 
 # Grab a reference to the file upload input element and add
